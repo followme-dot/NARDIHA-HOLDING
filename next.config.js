5526d6fed0e978710d2c882985074cd10b008495
Rafael Baena alvarez <info@ffollowme.com>
2026-01-10 21:25:54 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
     formats: ['image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
-  experimental: {
-    optimizeCss: true,
-  },
 }
 
 module.exports = nextConfig
